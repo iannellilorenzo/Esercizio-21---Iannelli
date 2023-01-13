351c929eeaf584fd117e4c1997508c323f08a283
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-01-13 12:17:46 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio 21 - Iannelli/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Dichiarazione 
+        string table = "<table>", tableC = "</table>";
+        string tr = "<tr>", trC = "</tr>";
+        string td = "<td>", tdC = "</td>";
+        int[] array = new int[100];
+        int index = 0, numUsati = 0;
+
+        numUsati = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < numUsati; i++)
+            array[i] = i;
+    }
+
+    static int Aggiunta(int[] array, int index)
+    {
+
+
+
+        return index;
+    }
+}
